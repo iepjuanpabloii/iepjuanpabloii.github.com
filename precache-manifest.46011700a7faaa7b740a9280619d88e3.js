da1027699c57bdc75bf1f9052ac74cee627a560d
Dennis <dennis.dair@gmail.com>
2020-06-12 11:30:37 -0500

    first release

---
+++ precache-manifest.46011700a7faaa7b740a9280619d88e3.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "e1be6a9d349394e590fbc85cf853bf0a",
+    "url": "/assets/icons/android-icon-144x144.png"
+  },
+  {
+    "revision": "fcaad8831fdd1b4f1ca99ce75773c810",
+    "url": "/assets/icons/android-icon-192x192.png"
+  },
+  {
+    "revision": "296a8c960fdd0b6f850f3d642c2782be",
+    "url": "/assets/icons/android-icon-36x36.png"
+  },
+  {
+    "revision": "3259c3c874e03ad8495ef305b14079aa",
+    "url": "/assets/icons/android-icon-48x48.png"
+  },
+  {
+    "revision": "7cb0e5ce491bbbc4ddf4418ecd559880",
+    "url": "/assets/icons/android-icon-72x72.png"
+  },
+  {
+    "revision": "eff9ddb769a0742b7680027eae064605",
+    "url": "/assets/icons/android-icon-96x96.png"
+  },
+  {
+    "revision": "9a21d27669877092147181cf52bbebc5",
+    "url": "/assets/icons/apple-icon-114x114.png"
+  },
+  {
+    "revision": "fc77a843c311579d58f5d96fc1a4c1b0",
+    "url": "/assets/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "e1be6a9d349394e590fbc85cf853bf0a",
+    "url": "/assets/icons/apple-icon-144x144.png"
+  },
+  {
+    "revision": "a0d3abcb0e3e28bd732719e952ff0e96",
+    "url": "/assets/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "ad4e745dcd12db644430afdaf6b4e564",
+    "url": "/assets/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "3d1a53b8b9c5ff42764564fc1dd17e92",
+    "url": "/assets/icons/apple-icon-57x57.png"
+  },
+  {
+    "revision": "4ef3b55e15ec0fe452f8611e20f46bc0",
+    "url": "/assets/icons/apple-icon-60x60.png"
+  },
+  {
+    "revision": "7cb0e5ce491bbbc4ddf4418ecd559880",
+    "url": "/assets/icons/apple-icon-72x72.png"
+  },
+  {
+    "revision": "a019e750f2d2c92fde03898fdbc12602",
+    "url": "/assets/icons/apple-icon-76x76.png"
+  },
+  {
+    "revision": "0c62b3de0fe42aa3d55d2193c62081a0",
+    "url": "/assets/icons/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "0c62b3de0fe42aa3d55d2193c62081a0",
+    "url": "/assets/icons/apple-icon.png"
+  },
+  {
+    "revision": "ad4e745dcd12db644430afdaf6b4e564",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "954911446641c9a5582cb04d6c5d0406",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a9a0e13afbc50e9a9350ef8673b4bf55",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "eff9ddb769a0742b7680027eae064605",
+    "url": "/assets/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "e1be6a9d349394e590fbc85cf853bf0a",
+    "url": "/assets/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "2b721655ecd4b469c827ebb645ba876f",
+    "url": "/assets/icons/ms-icon-310x310.png"
+  },
+  {
+    "revision": "06954d1eaa263c62b45d99fd1c0df9a2",
+    "url": "/assets/icons/ms-icon-70x70.png"
+  },
+  {
+    "revision": "7027c47591c015a327a8dde35df5271d",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "655a990ff6f04ae59b83d4d16a44d949",
+    "url": "/assets/images/book-covers/book-101-dalmatas.png"
+  },
+  {
+    "revision": "536cb35786d418709d0207bdc4eadaad",
+    "url": "/assets/images/book-covers/book-20000leguas-viaje.png"
+  },
+  {
+    "revision": "a75b2254efe258e0e005c923d49f92e2",
+    "url": "/assets/images/book-covers/book-alicia-pais-maravillas.png"
+  },
+  {
+    "revision": "51280f842b191e396ae50a1c4922a6f2",
+    "url": "/assets/images/book-covers/book-bambi.png"
+  },
+  {
+    "revision": "0fcdadc2c7105199b1760ca55fec624e",
+    "url": "/assets/images/book-covers/book-basil-detective.png"
+  },
+  {
+    "revision": "31c34f152f6522c715bcde0687af5b54",
+    "url": "/assets/images/book-covers/book-blancanieves.png"
+  },
+  {
+    "revision": "f57ed99bae1b33efd0094ef17e08fde7",
+    "url": "/assets/images/book-covers/book-dumbo.png"
+  },
+  {
+    "revision": "702a23f777c5e7c0332330b00a012ec9",
+    "url": "/assets/images/book-covers/book-hercules.png"
+  },
+  {
+    "revision": "dd4fd9d2754268be199a994c6f33fae9",
+    "url": "/assets/images/book-covers/book-libro-selva.png"
+  },
+  {
+    "revision": "0c5914cc2435b58676cba869c446ba6b",
+    "url": "/assets/images/book-covers/book-mulan.png"
+  },
+  {
+    "revision": "0279cba0ec3d20ebee6143b6f6ac5def",
+    "url": "/assets/images/book-covers/book-nemo.png"
+  },
+  {
+    "revision": "bbb61ac8bfdde09117ca2701a8e1b44a",
+    "url": "/assets/images/book-covers/book-rey-leon.png"
+  },
+  {
+    "revision": "896b5117cb3b55c7c855bd95489c9cfb",
+    "url": "/assets/images/book-covers/book-sirenita.png"
+  },
+  {
+    "revision": "d708f0bf5d17e7dc2c9c07569a1ad366",
+    "url": "/assets/images/kid-computer.jpg"
+  },
+  {
+    "revision": "1295271dd5bd388a278630c5aef728fc",
+    "url": "/assets/images/logoJuanPabloII.png"
+  },
+  {
+    "revision": "5c1123a55e2683fc4527",
+    "url": "/bundle.5c112.js"
+  },
+  {
+    "revision": "5c1123a55e2683fc4527",
+    "url": "/bundle.9f4d1.css"
+  },
+  {
+    "revision": "b39b67fc464741593b58422917e41f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "544598970c91001c8811",
+    "url": "/polyfills.54459.js"
+  },
+  {
+    "revision": "b14b6d2328e78fd6ef21",
+    "url": "/route-biblioteca.chunk.b14b6.js"
+  },
+  {
+    "revision": "8d7aecffe76d0b041c8e",
+    "url": "/route-clases.chunk.8d7ae.js"
+  },
+  {
+    "revision": "8d7aecffe76d0b041c8e",
+    "url": "/route-clases.chunk.9ad51.css"
+  },
+  {
+    "revision": "342751fed97359dea7ed",
+    "url": "/route-clases~route-login.chunk.34275.js"
+  },
+  {
+    "revision": "dafffcaf9b9397179bda",
+    "url": "/route-comunicados.chunk.01579.css"
+  },
+  {
+    "revision": "dafffcaf9b9397179bda",
+    "url": "/route-comunicados.chunk.dafff.js"
+  },
+  {
+    "revision": "13ea82dc3707922652f3",
+    "url": "/route-home.chunk.13ea8.js"
+  },
+  {
+    "revision": "13ea82dc3707922652f3",
+    "url": "/route-home.chunk.8b31d.css"
+  },
+  {
+    "revision": "91cd3913904f344aeac8",
+    "url": "/route-login.chunk.91cd3.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "f49f8e16dd0faff1e316074323b97f87",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
